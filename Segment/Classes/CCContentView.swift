8b8438f94b990f65cbf7da2b7b636dcb0f84c27f
cillyfly <cillyfly@163.com>
2018-06-01 15:06:04 +0800

    增加内容

---
+++ Segment/Classes/CCContentView.swift
@@ -0,0 +1,20 @@
+//
+//  CCContentView.swift
+//  Pods-Segment_Example
+//
+//  Created by cc x on 2018/6/1.
+//
+
+import UIKit
+
+class CCContentView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
